885f75d96ed1f01379615711716bc2a30d8ed3a8
Marcos <marcos.marcondes@sou.inteli.edu.br>
2025-04-16 23:02:52 -0300

    Configures Phaser for the project

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { Game as MainGame } from "./scenes/Game";
+import { AUTO, Scale, Game } from "phaser";
+
+const config = {
+  type: AUTO,
+  width: 1280,
+  height: 720,
+  parent: "game-container",
+  backgroundColor: "#000000",
+  scale: {
+    mode: Scale.FIT,
+    autoCenter: Scale.CENTER_BOTH,
+  },
+  scene: [MainGame],
+};
+
+export default new Game(config);
